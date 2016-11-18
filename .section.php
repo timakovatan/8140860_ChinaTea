9735e49904a1e72a885e9e0f0298b431c4defab1
Tanya Timakova <tanucha19@gmail.com>
2016-11-18 18:37:58 +0300

    git-repository creation

---
+++ .section.php
@@ -0,0 +1,8 @@
+<?
+$sSectionName = "Главная страница";
+$arDirProperties = array(
+   "description" => "Мы предлагаем широкий ассортимент китайского чая по адекватным ценам.",
+   "keywords" => "китайский чай",
+   "robots" => "index, follow"
+);
+?>
