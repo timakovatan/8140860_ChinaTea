2ee5e1b084ee40303213e247cb0caaeb2d225521
Tanya Timakova <tanucha19@gmail.com>
2016-12-15 18:21:52 +0300

    DAO TEAS department has been created

--- local/modules/miet.kpi/lib/teasdepartment.php
+++ local/modules/miet.kpi/lib/teasdepartment.php
@@ -1,9 +1,9 @@
 <?php
-namespace MIET\KPI;
+namespace MIET\TEAS;
 use Bitrix\Main\Entity;
 use Bitrix\Main\Localization\Loc;
 Loc::loadMessages(__FILE__);
-class KPIDepartmentTable extends Entity\DataManager {
+class TEASDepartmentTable extends Entity\DataManager {
     public static function getFilePath()
     {
         return __FILE__;
@@ -11,9 +11,9 @@ class KPIDepartmentTable extends Entity\DataManager {
     /*Название таблицы HL в БД*/
     public static function getTableName()
     {
-        return 't_department_kpi';
+        return 't_department_teas';
     }
-    /*Описание полей сущности (соответсвуют полям HL DepartmentKPI)*/
+    /*Описание полей сущности (соответсвуют полям HL DepartmentTEAS)*/
     public static function getMap()
     {
         return array(
@@ -21,11 +21,11 @@ class KPIDepartmentTable extends Entity\DataManager {
                 'data_type' => 'integer',
                 'primary' => true,
                 'autocomplete' => true,
-                'title' => Loc::getMessage('KPI_ENTITY_ID_FIELD')
+                'title' => Loc::getMessage('TEAS_ENTITY_ID_FIELD')
             ),
-            'UF_KPI' => array(
+            'UF_TEAS' => array(
                 'data_type' => 'integer',
-                'required' => true,'title' => Loc::getMessage('KPI_ENTITY_UF_KPI_FIELD')
+                'required' => true,'title' => Loc::getMessage('TEAS_ENTITY_UF_TEAS_FIELD')
             ),
             'UF_VALUE' => array(
                 'data_type' => 'float',
@@ -35,46 +35,46 @@ class KPIDepartmentTable extends Entity\DataManager {
  'validateValue' //Название метода-валидатора вданном классе
  ),
 'title' =>
-Loc::getMessage('KPI_ENTITY_UF_VALUE_FIELD')
+Loc::getMessage('TEAS_ENTITY_UF_VALUE_FIELD')
  ),
  'UF_DEPARTMENT' => array(
         'data_type' => 'integer',
         'required' => true,
         'title' =>
-            Loc::getMessage('KPI_ENTITY_UF_DEPARTMENT_FIELD')
+            Loc::getMessage('TEAS_ENTITY_UF_DEPARTMENT_FIELD')
     ),
  'UF_CREATED_BY' => array(
         'data_type' => 'integer',
         'required' => true,
         'title' =>
-            Loc::getMessage('KPI_ENTITY_UF_CREATED_BY_FIELD')
+            Loc::getMessage('TEAS_ENTITY_UF_CREATED_BY_FIELD')
     ),
  'UF_CREATED' => array(
         'data_type' => 'datetime',
         'required' => true,
         'title' =>
-            Loc::getMessage('KPI_ENTITY_UF_CREATED_FIELD')
+            Loc::getMessage('TEAS_ENTITY_UF_CREATED_FIELD')
     ),
  'UF_CHANGED_BY' => array(
         'data_type' => 'integer',
         'title' =>
-            Loc::getMessage('KPI_ENTITY_UF_CHANGED_BY_FIELD')
+            Loc::getMessage('TEAS_ENTITY_UF_CHANGED_BY_FIELD')
     ),
  'UF_CHANGED' => array(
         'data_type' => 'datetime',
         'title' =>
-            Loc::getMessage('KPI_ENTITY_UF_CHANGED_FIELD'),
+            Loc::getMessage('TEAS_ENTITY_UF_CHANGED_FIELD'),
     ),
  'UF_PERIOD' => array(
         'data_type' => 'date',
         'title' =>
-            Loc::getMessage('KPI_ENTITY_UF_PERIOD_FIELD'),
+            Loc::getMessage('TEAS_ENTITY_UF_PERIOD_FIELD'),
     ),
  //Описываем все связи с другими таблицами (внешние ключи)
  new Entity\ReferenceField(
-     'UF_KPI',
+     'UF_TEAS',
      'Bitrix\Iblock\ElementTable',
-     array('=this.UF_KPI' => 'ref.ID')
+     array('=this.UF_TEAS' => 'ref.ID')
  ),
  new Entity\ReferenceField(
      'UF_DEPARTMENT',
